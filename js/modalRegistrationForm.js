95abb1ade5658b9e54516f39613c105d06be50b1
f0xx1996 <ph.box@mail.ru>
2018-11-29 01:24:40 +0400

    client part
    clent part

--- js/modalRegistrationForm.js
+++ js/modalRegistrationForm.js
@@ -1,42 +1,96 @@
-// var email = document.querySelector("#modal-email-reg");
-// var login = document.querySelector("#modal-login-reg");
-// var name = document.querySelector("#modal-name-reg");
-// var surname = document.querySelector("#modal-surname-reg");
-// var password = document.querySelector("#modal-password-reg");
-// var comfirePassword = document.querySelector("#modal-comfire-password-reg");
-// var birthday = document.querySelector("#modal-date-reg");
-// var country = document.querySelector("#select-box1");
-// var number = document.querySelector("#modal-phone-reg");
-// var cancelBtn = document.querySelector("#cancel-registration-btn");
-// var closeBtn = document.querySelector(".close-button");
-// var modal = document.querySelector(".modal");
+var emailModal = document.getElementById("modal-email-reg");
+var loginModal = document.getElementById("modal-login-reg");
+var nameModal = document.getElementById("modal-name-reg");
+var surnameModal = document.getElementById("modal-surname-reg");
+var passwordModal = document.getElementById("modal-password-reg");
+var comfirePasswordModal = document.getElementById("modal-comfire-password-reg");
+var birthdayModal = document.getElementById("modal-date-reg");
+var countryModal = document.getElementById("select-box1");
+var numberModal = document.getElementById("modal-phone-reg");
+var cancelBtnModal = document.getElementById("cancel-registration-btn");
+
+var closeBtn = document.querySelector(".close-button");
+var modal = document.querySelector(".modal");
 
 
-// var emailError = document.querySelector("#modal-mail-error");
-// var loginError = document.querySelector("#modal-login-error");
-// var nameError = document.querySelector("#modal-name-error");
-// var surnameError = document.querySelector("#modal-surname-error");
-// var passwordError = document.querySelector("#modal-password-error");
-// var birthdayError = document.querySelector("#modal-date-error");
-// var countryError = document.querySelector("#select-box1");
-// var numberError = document.querySelector("#modal-phone-error");
+var emailErrorModal = document.getElementById("modal-email-error");
+var loginErrorModal = document.getElementById("modal-login-error");
+var nameErrorModal = document.getElementById("modal-name-error");
+var surnameErrorModal = document.getElementById("modal-surname-error");
+var passwordErrorModal = document.getElementById("modal-password-error");
+var birthdayErrorModal = document.getElementById("modal-date-error");
+var countryErrorModal = document.getElementById("select-box1");
+var numberErrorModal = document.getElementById("modal-phone-error");
 
-// var emailRegexp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+var emailRegexp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-// closeBtn.addEventListener("click", function() {
-//     modal.classList.toggle("show-modal");
-// });
+closeBtn.addEventListener("click", function() {
+    modal.classList.toggle("show-modal");
+});
 
-// window.addEventListener("click", function(event) {
-//     if (event.target === modal) {
-//         toggleModal();
-//     }
-// });
+window.addEventListener("click", function(event) {
+    if (event.target === modal) {
+        toggleModal();
+    }
+});
 
-// cancelBtn.addEventListener("click", function() {
-//     modal.classList.toggle("show-modal");
-// });
+cancelBtnModal.addEventListener("click", function() {
+    modal.classList.toggle("show-modal");
+});
 
-// function toggleModal() {
-//     modal.classList.toggle("show-modal");
-// }
+function toggleModal() {
+    modal.classList.toggle("show-modal");
+}
+
+function checkNewUser() {
+    var flag = true;
+    if (emailModal.value == '' || !emailRegexp.test(emailModal.value)) {
+        emailModal.style.border = '1x solid red';
+        emailErrorModal.textContent = '*E-mail is wrong';
+        emailModal.focus();
+        flag = false;
+    }
+    if (loginModal === '') {
+        loginModal.style.border = '1x solid red';
+        loginErrorModal.textContent = '*Input your login';
+        loginModal.focus();
+        flag = false;
+    }
+    if (nameModal === '') {
+        nameModal.style.border = '1x solid red';
+        nameErrorModal.textContent = '*Input your name';
+        nameModal.focus();
+        flag = false;
+    }
+    if (surnameModal === '') {
+        surnameModal.style.border = '1x solid red';
+        surnameErrorModal.textContent = '*Input your surname';
+        surnameModal.focus();
+        flag = false;
+    }
+    if (passwordModal.value == '' || passwordModal.value.length < 5) {
+        passwordModal.style.border = '1x solid red';
+        passwordErrorModal.textContent = '*Your password must contain at leasts 5 characters';
+        passwordModal.focus();
+        flag = false;
+    }
+    if (new Date(birthdayModal.value) > Date.getNow()) {
+        birthdayModal.style.border = '1x solid red';
+        birthdayErrorModal.textContent = '*Incorrect date birthday';
+        birthdayModal.focus();
+        flag = false;
+    }
+    if (new Date(birthdayModal.value) > Date.getNow()) {
+        birthdayModal.style.border = '1x solid red';
+        birthdayErrorModal.textContent = '*Incorrect date birthday';
+        birthdayModal.focus();
+        flag = false;
+    }
+    if (flag) {
+        alert('Вы авторизовались, как пользователь ' + email.value);
+        return true;
+    } else {
+        alert('Вы не можете авторизоваться!\nПроверьте правильность введенных данных.');
+        return false;
+    }
+}
--- js/signIn.js
+++ js/signIn.js
@@ -1,39 +1,39 @@
-var registrationBtn = document.querySelector("#btn-registration");
+var registrationBtn = document.getElementById('registration-btn');
 var modal = document.querySelector(".modal");
 
 registrationBtn.addEventListener("click", function() {
     modal.classList.toggle("show-modal");
 });
 
-var email = document.querySelector('#user-email-input');
-var password = document.querySelector('#user-password-input');
+var email = document.getElementById('signIn-email');
+var password = document.getElementById('signIn-password');
 
-var signIn = document.querySelector("#sign-in");
+var signIn = document.getElementById("signIn-btn");
 
 var emailRegexp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-var emailError = document.querySelector("#mail-error");
-var passwordError = document.querySelector("#password-error");
+var emailError = document.getElementById("signIn-email-error");
+var passwordError = document.getElementById("signIn-password-error");
 
 function checkUser() {
     var flag = true;
     if (email.value == '' || !emailRegexp.test(email.value)) {
         email.style.border = '1x solid red';
-        emailError.textContent = '*Email is wrong';
+        emailError.textContent = '*E-mail is wrong';
         email.focus();
         flag = false;
     }
     if (password.value == '') {
         password.style.border = '1x solid red';
-        passwordError.textContent = '*Password is wrong';
+        passwordError.textContent = '*Input your password';
         password.focus();
         flag = false;
     }
     if (flag) {
-        alert('yes');
+        alert('Вы авторизовались, как пользователь ' + email.value);
         return true;
     } else {
-        alert('no');
+        alert('Вы не можете авторизоваться!\nПроверьте правильность введенных данных.');
         return false;
     }
 }
@@ -41,7 +41,7 @@ function checkUser() {
 email.onblur = function() {
     if (email.value == '' || !emailRegexp.test(email.value)) {
         email.style.border = '1px solid red';
-        emailError.textContent = 'Email is wrong';
+        emailError.textContent = '*E-mail is wrong';
     } else if (email.value != '') {
         email.style.border = "1px solid black";
         emailError.innerHTML = '';
@@ -51,26 +51,10 @@ email.onblur = function() {
 password.onblur = function() {
     if (password.value == '') {
         password.style.border = '1px solid red';
-        passwordError.textContent = 'Password is wrong';
+        passwordError.textContent = '*Input your password';
     }
     if (password.value != '') {
         password.style.border = "1px solid black";
         passwordError.innerHTML = '';
     }
 };
-
-function emailVerify() {
-    if (email.value != '') {
-        email.style.border = "1px solid black";
-        emailError.innerHTML = '';
-        return true;
-    }
-}
-
-function passwordVerify() {
-    if (password.value != '') {
-        password.style.border = "1px solid black";
-        passwordError.innerHTML = '';
-        return true;
-    }
-}
